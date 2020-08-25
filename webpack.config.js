5bf981d05a933f712ea4a0dce71011418229b0f3
rearalf <rear_recinos@hotmail.com>
2020-08-24 20:20:58 -0600

    Change the favicon

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: './public/index.html',
 			filename: './index.html',
-			favicon: './public/logo.jpg',
+			favicon: './public/favicon.png',
 		}),
 		new MiniCssExtractPlugin({
 			filename: 'assets/[name].css',
